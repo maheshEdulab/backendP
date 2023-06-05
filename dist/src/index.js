781f935d63e5901758fe2837c147b28ffd0bb536
Mahesh Prajapati <mahesh@edulab.in>
2023-06-05 18:59:55 +0530

    Second commit

--- dist/src/index.js
+++ dist/src/index.js
@@ -21,30 +21,90 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 databse_1.default.initialize().then(() => {
-    console.log("DataBase of person  connected ");
+    console.log("DataBase connected ");
 });
-app.get('/read', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const record = yield entity_1.Persons.find();
-    res.json({ record, Messege: "Welcome" });
-}));
-app.get('/verfy', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const person = yield databse_1.default.manager.findOneBy(entity_1.Persons, { UserName: JSON.stringify(req.query.UserName), Password: JSON.stringify(req.query.Password) });
-    if (person) {
-        console.log(person);
-        res.json({ person, Messege: "Record get" });
+app.post('/create', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        console.log(req.body);
+        console.log(req.body.Id);
+        jsonwebtoken_1.default.sign(req.body, req.body.Id, { expiresIn: '24h' }, (err, token) => __awaiter(void 0, void 0, void 0, function* () {
+            if (token) {
+                yield databse_1.default
+                    .createQueryBuilder()
+                    .insert()
+                    .into(entity_1.Persons)
+                    .values([Object.assign(Object.assign({}, req.body), { Token: token })])
+                    .execute();
+                res.json("Data Inserted into table");
+            }
+            else {
+                res.json({ err, Messege: "Error in fatching in token " });
+            }
+        }));
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in createing data " });
     }
 }));
-app.post('/create', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    jsonwebtoken_1.default.sign(req.body, req.body.Password, { expiresIn: '2' }, (err, token) => __awaiter(void 0, void 0, void 0, function* () {
-        console.log(token);
+app.get('/read', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const record = yield entity_1.Persons.find();
+        res.json({ record, Messege: "Welcome" });
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in Reading data " });
+    }
+}));
+app.get('/verfy', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const person = yield databse_1.default.manager.findOneBy(entity_1.Persons, { UserName: req.query.UserName, Password: req.query.Password });
+        if (person) {
+            const decode = jsonwebtoken_1.default.verify(person.Token, person.Password);
+            console.log(decode);
+            res.json({ Messege: true, Token: person.Token });
+        }
+        else {
+            res.json({ Messege: false });
+        }
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in verfying data " });
+    }
+}));
+app.put('/update/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const person = yield databse_1.default.manager.findOneBy(entity_1.Persons, { Id: parseInt(req.params.id) });
+        if (person) {
+            yield databse_1.default
+                .createQueryBuilder()
+                .update(entity_1.Persons)
+                .set(req.body)
+                .where("id = :id", { id: req.params.id })
+                .execute();
+            res.json({ Messege: "Successfull" });
+        }
+        else {
+            res.json({ Messege: "Unsuccessfull" });
+        }
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in verfying data " });
+    }
+}));
+app.delete('/delete/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const record = yield databse_1.default.manager.findOneBy(entity_1.Persons, { Id: parseInt(req.params.id) });
         yield databse_1.default
             .createQueryBuilder()
-            .insert()
-            .into(entity_1.Persons)
-            .values([Object.assign(Object.assign({}, req.body), { Token: token })])
+            .delete()
+            .from(entity_1.Persons)
+            .where({ Id: req.params.id })
             .execute();
-        res.json("Data Inserted into table");
-    }));
+        res.json(record);
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in verfying data " });
+    }
 }));
 app.listen(4000, () => {
     console.log("Server listening on port 4000");
--- src/index.ts
+++ src/index.ts
@@ -10,34 +10,96 @@ app.use(cors());
 app.use(express.json());
 
 dataSource.initialize().then(() => {
-    console.log("DataBase of person  connected ")
-})
-
-
-app.get('/read', async (req: Request, res: Response) => {
-    const record = await Persons.find()
-    res.json({ record, Messege: "Welcome" })
-})
-
-app.get('/verfy', async (req: Request, res: Response) => {
-    console.log(req.query)
-    const person = await dataSource.manager.findOneBy(Persons, { UserName: req.query.UserName as string, Password: req.query.Password as string })
-    res.json(person?.Token)
+    console.log("DataBase connected ")
 })
 
 app.post('/create', async (req: Request, res: Response) => {
-    jwt.sign(req.body, req.body.Password, { expiresIn: '2' }, async (err, token) => {
-        console.log(token)
-        await dataSource
-            .createQueryBuilder()
-            .insert()
-            .into(Persons)
-            .values([{ ...req.body, Token: token }])
-            .execute()
-        res.json("Data Inserted into table")
-    })
+    try {
+        console.log(req.body)
+        console.log(req.body.Id)
+        jwt.sign(req.body, req.body.Id, { expiresIn: '24h' }, async (err, token) => {
+            if (token) {
+                await dataSource
+                    .createQueryBuilder()
+                    .insert()
+                    .into(Persons)
+                    .values([{ ...req.body, Token: token }])
+                    .execute()
+                res.json("Data Inserted into table")
+            } else {
+                res.json({ err, Messege: "Error in fatching in token " })
+            }
+
+        })
+
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in createing data " })
+    }
 })
 
+app.get('/read', async (req: Request, res: Response) => {
+    try {
+        const record = await Persons.find()
+        res.json({ record, Messege: "Welcome" })
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in Reading data " })
+    }
+})
+
+app.get('/verfy', async (req: Request, res: Response) => {
+    try {
+        const person = await dataSource.manager.findOneBy(Persons, { UserName: req.query.UserName as string, Password: req.query.Password as string })
+        if (person) {
+            const decode = jwt.verify(person.Token, person.Password)
+            console.log(decode)
+            res.json({ Messege: true, Token: person.Token })
+        } else {
+            res.json({ Messege: false })
+        }
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in verfying data " })
+    }
+
+})
+
+app.put('/update/:id', async (req: Request, res: Response) => {
+    try {
+        const person = await dataSource.manager.findOneBy(Persons, { Id: parseInt(req.params.id) })
+        if (person) {
+            await dataSource
+                .createQueryBuilder()
+                .update(Persons)
+                .set(req.body)
+                .where("id = :id", { id: req.params.id })
+                .execute()
+            res.json({ Messege: "Successfull" })
+        } else {
+            res.json({ Messege: "Unsuccessfull" })
+        }
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in verfying data " })
+    }
+})
+
+app.delete('/delete/:id', async (req: Request, res: Response) => {
+    try {
+        const record = await dataSource.manager.findOneBy(Persons, { Id: parseInt(req.params.id) })
+        await dataSource
+            .createQueryBuilder()
+            .delete()
+            .from(Persons)
+            .where({ Id: req.params.id })
+            .execute();
+        res.json(record)
+    }
+    catch (error) {
+        res.json({ error, Messege: "Someting went worong in verfying data " })
+    }
+})
 
 app.listen(4000, () => {
     console.log("Server listening on port 4000")
